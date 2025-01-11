a0c63bf0b5d92d6aace80a9a26d81611356e3cf2
Xavier Kooijman <xavierkooijman@gmail.com>
2025-01-11 16:21:17 +0000

    link underline animation, navbar component setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup>
+<script>
 import { RouterLink } from 'vue-router';
+import navbar from "@/components/navbar.vue";
+
+export default {
+    components: {
+        navbar,
+    }
+}
 
 </script>
 
 <template>
+    <navbar />
     <RouterView />
 </template>
 
---
+++ src/components/navbar.vue
@@ -0,0 +1,11 @@
+<script>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="header">
+    <RouterLink class="hover-underline-animation" :to="{ name: 'HomePage' }">Hypnøtica</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>

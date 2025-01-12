1f60847246c98a24ca46c4827ef6a24b06275a3e
Xavier Kooijman <xavierkooijman@gmail.com>
2025-01-12 19:50:15 +0000

    navbar animation, open and close not smooth

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -24,47 +24,47 @@ export default {
 <template>
   <div class="navbar" :class="{changePadding: open}">
     <div class="navbar-menuLinks">
-      <div class="navbar-menu" @click="navbarAnimation()">
+      <div :class="{w100: open}" class="navbar-menu" @click="navbarAnimation()">
         <div class="menu-btn">
           <img src="/src/assets/Icons/MenuIcon.svg" alt="">
           <p class="menu-text">Menu</p>
         </div>
         <img class="display" :class="{flex: open}" src="/src/assets/Icons/CrossXIcon.svg" alt="">
       </div>
-      <div class="navbar-links">
+      <div class="navbar-links" :class="{display: open}">
         <RouterLink class="link" :to="{ name: 'TicketsPage' }">Tickets</RouterLink>
         <RouterLink class="link" :to="{ name: 'ProgramEventsPage' }">Program</RouterLink>
         <RouterLink class="link" :to="{ name: 'ProgramArtistsPage' }">Artists</RouterLink>
         <RouterLink class="link" :to="{ name: 'AboutUsPage' }">About us</RouterLink>
       </div>
     </div>
-    <div class="hidden-container" :class="{flex: open}">
-      <div class="hidden-section" :class="{flex: open}">
+    <div class="hidden-container" :class="{subnav: open}">
+      <div class="hidden-section" :class="{vShow: open}">
         <p class="hidden-title">Festival</p>
         <div class="hidden-links">
-          <RouterLink class="hover-underline-animation" :to="{ name: 'TicketsPage' }">Tickets</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'ProgramEventsPage' }">Program</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'ProgramArtistsPage' }">Artists</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'ProgramVenuePage' }">Venues</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'TicketsPage' }">Tickets</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'ProgramEventsPage' }">Program</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'ProgramArtistsPage' }">Artists</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'ProgramVenuePage' }">Venues</RouterLink>
         </div>
       </div>
-            <div class="hidden-section" :class="{flex: open}">
+            <div class="hidden-section" :class="{vShow: open}">
         <p class="hidden-title">Practical</p>
         <div class="hidden-links">
-          <RouterLink class="hover-underline-animation" :to="{ name: 'LocationsMapPage' }">Locations map</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'TravelPage' }">Travel</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'WhereToStayPage' }">Where to stay</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'FAQPage' }">FAQ</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'LocationsMapPage' }">Locations map</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'TravelPage' }">Travel</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'WhereToStayPage' }">Where to stay</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'FAQPage' }">FAQ</RouterLink>
         </div>
       </div>
-      <div class="hidden-section" :class="{flex: open}">
+      <div class="hidden-section" :class="{vShow: open}">
         <p class="hidden-title">More</p>
         <div class="hidden-links">
-          <RouterLink class="hover-underline-animation" :to="{ name: 'NewsPage' }">News</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'ContactUsPage' }">Contact us</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'VolunteerPage' }">Volunteer</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'MerchandisingPage' }">Merchandise</RouterLink>
-          <RouterLink class="hover-underline-animation" :to="{ name: 'AboutUsPage' }">About us</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'NewsPage' }">News</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'ContactUsPage' }">Contact us</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'VolunteerPage' }">Volunteer</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'MerchandisingPage' }">Merchandise</RouterLink>
+          <RouterLink class="hover-underline-animation w-fit" :to="{ name: 'AboutUsPage' }">About us</RouterLink>
         </div>
       </div>
     </div>
@@ -73,9 +73,23 @@ export default {
 
 <style scoped>
 
+.w-fit{
+  width: fit-content;
+}
+
+.w100{
+  width: 100%;
+  transition: all 1s linear allow-discrete;
+}
+
 .flex{
   display: flex !important;
 }
+
+.vShow{
+  visibility: visible !important;
+}
+
 
 .changePadding{
   padding: 6px !important;
@@ -83,11 +97,9 @@ export default {
 
 .navbar{
   display: flex;
-  width: fit-content;
+  width: 588px;
   padding: 6px 16px 6px 6px;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
   gap: 12px;
   border-radius: 10px;
   background: rgba(250, 250, 250, 0.05);
@@ -104,6 +116,7 @@ export default {
 .navbar-links{
   display: flex;
   flex-direction: row;
+  align-items: center;
   gap: 48px;
 }
 
@@ -115,11 +128,13 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   border-radius: 8px;
   background: rgba(250, 250, 250, 0.06);
   backdrop-filter: blur(100px);
   padding: 12px;
   cursor: pointer;
+  transition: all 1s linear allow-discrete;
 }
 
 .menu-btn{
@@ -129,14 +144,14 @@ export default {
 }
 
 .menu-text{
-  font-family: Aspekta500;
-  font-size: 1.3rem;
+  font-family: Aspekta400;
+  font-size: 1.25rem;
 }
 
 .link{
   color: var(--gray200);
-  font-family: Aspekta500;
-  font-size: 1.3rem;
+  font-family: Aspekta400;
+  font-size: 1.25rem;
 }
 
 .link:hover{
@@ -157,19 +172,29 @@ export default {
 }
 
 .hidden-section{
-  display: none;
+  visibility: hidden;
+  display: flex;
   flex-direction: column;
   gap: 16px;
 }
 
 .hidden-container{
   display: none;
+  max-height: 0;
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
-  padding: 24px;
   border-radius: 8px;
   background: rgba(250, 250, 250, 0.02);
   backdrop-filter: blur(100px);
+  transition: all 1s linear allow-discrete;
 }
+
+.subnav{
+  display: flex;
+  max-height: fit-content;
+  padding: 24px;
+  transition: all 1s linear allow-discrete;
+}
+
 </style>
